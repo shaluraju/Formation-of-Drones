12d480b8d7d228c4756a69fa88e68fe9b503dae3
Shalemuraju <67613439+shaluraju@users.noreply.github.com>
2021-12-02 13:49:55 -0500

    Update Multiple_Formation.py

--- scripts/Multiple_Formation.py
+++ scripts/Multiple_Formation.py
@@ -15,18 +15,22 @@ import matplotlib.pyplot as plt
 
 #import timeit
 
-wifi_interfaces = ["wls1",
-                   "wlx9cefd5fb6d70",
-                   "wlx9cefd5fb6f40",
-                    "wlx9cefd5fb71cc",
-                   #"wlx9cefd5fb774c",
+wifi_interfaces = ["wlx9cefd5fb6d70",
+                   "wlx9cefd5fb71cc",
+                   "wlx9cefd5fb774c",
+                    "wlx9cefd5fb6a56",
+                   "wlx9cefd5fae98b",
+                   "wlx9cefd5fb6d84",
+                   "wlx9cefd5faec05",
                    ]
 
-droneslist = ['Tello6',
-              'Tello7',
-              'Tello8',
+droneslist = ['Tello3',
               'Tello4',
-              #'Tello8',
+              'Tello5',
+              'Tello6',
+              'Tello8',
+              'Tello9',
+              'Tello10',
              ]
 
 
@@ -86,7 +90,7 @@ drone3PIDz = PID(Kp=300, Kd=300, Ki=0.0,
                   minOutput = -70, maxOutput = 70,
                   current_time = None)
 
-#---------------------------------------------------------------
+#------------------------------------------------        
 
 drone4PIDx = PID(Kp=300, Kd=300, Ki=0.0,
                   derivativeFilterFreq=15,
@@ -104,18 +108,66 @@ drone4PIDz = PID(Kp=300, Kd=300, Ki=0.0,
                   current_time = None)
 
 #---------------------------------------------------------------
+drone5PIDx = PID(Kp=300, Kd=300, Ki=0.0,
+                  derivativeFilterFreq=10,
+                  minOutput = -70, maxOutput = 70,
+                  current_time = None)
 
-#drone5PIDx = PID(Kp=300, Kd=300, Ki=0.0,
+drone5PIDy = PID(Kp=400, Kd=170, Ki=0.0,
+                  derivativeFilterFreq=10,
+                  minOutput = -70, maxOutput = 70,
+                  current_time = None)
+
+drone5PIDz = PID(Kp=300, Kd=300, Ki=0.0,
+                  derivativeFilterFreq=10,
+                  minOutput = -70, maxOutput = 70,
+                  current_time = None)
+
+#---------------------------------------------------------------
+drone6PIDx = PID(Kp=300, Kd=300, Ki=0.0,
+                  derivativeFilterFreq=10,
+                  minOutput = -70, maxOutput = 70,
+                  current_time = None)
+
+drone6PIDy = PID(Kp=400, Kd=170, Ki=0.0        ,
+                  derivativeFilterFreq=10,
+                  minOutput = -70, maxOutput = 70,
+                  current_time = None)
+
+drone6PIDz = PID(Kp=300, Kd=300, Ki=0.0,
+                  derivativeFilterFreq=10,
+                  minOutput = -70, maxOutput = 70,
+                  current_time = None)
+
+#--------------------------------------------------------------
+drone7PIDx = PID(Kp=300, Kd=300, Ki=0.0,
+                  derivativeFilterFreq=10,
+                  minOutput = -70, maxOutput = 70,
+                  current_time = None)
+
+drone7PIDy = PID(Kp=400, Kd=170, Ki=0.0,
+                  derivativeFilterFreq=10,
+                  minOutput = -70, maxOutput = 70,
+                  current_time = None)
+
+drone7PIDz = PID(Kp=300, Kd=300, Ki=0.0,
+                  derivativeFilterFreq=10,
+                  minOutput = -70, maxOutput = 70,
+                  current_time = None)
+
+#---------------------------------------------------------------
+
+#drone8PIDx = PID(Kp=300, Kd=300, Ki=0.0,
 #                  derivativeFilterFreq=15,
 #                  minOutput = -70, maxOutput = 70,
 #                  current_time = None)
 #
-#drone5PIDy = PID(Kp=400, Kd=170, Ki=0.0,
+#drone8PIDy = PID(Kp=400, Kd=170, Ki=0.0,
 #                  derivativeFilterFreq=15,
 #                  minOutput = -70, maxOutput = 70,
 #                  current_time = None)
 #
-#drone5PIDz = PID(Kp=300, Kd=300, Ki=0.0,
+#drone8PIDz = PID(Kp=300, Kd=300, Ki=0.0,
 #                  derivativeFilterFreq=15,
 #                  minOutput = -70, maxOutput = 70,
 #                  current_time = None)
@@ -145,7 +197,7 @@ print("Initial_pos: ", pos_curr)
 goal_pos = pos_curr
 
 try: 
-    while j < 3600:
+    while j < 2500:
         
         location = path_planning.Hover(pos_curr, goal_pos)
         
@@ -160,17 +212,15 @@ try:
         
         if j > 600 and j < 1500:
             
-            location = Character.form_I(pos_curr, [0.4, 0, 0.4], min_dist = [0.55,0.55,0.4])
+            location = Character.form_S(pos_curr, [0.0, 0.0, 1.0])
 
         if j > 1499 and j < 2500:
             
-            location = Character.form_Square(pos_curr, [0.2,-0.2,0.5])
+            location = Character.form_A(pos_curr, [0.2,-0.2,0.5])
 
-        if j >2499 and j < 3200:
 
-            location = Character.form_L(pos_curr, [0.3, -0.2, 0.5], min_dist = [0.6,0.6,0.4])
         
-        print("location: ", location)  
+        print("Goal: ", location)  
         
         
         
@@ -192,6 +242,27 @@ try:
         PIDvx4 = drone4PIDx.update(pos_curr[3][0], location[3][0])
         PIDvy4 = drone4PIDy.update(pos_curr[3][1], location[3][1])
         PIDvz4 = drone4PIDz.update(pos_curr[3][2], location[3][2])
+        
+        #---------------------------------------------------------
+        
+        PIDvx5 = drone5PIDx.update(pos_curr[4][0], location[4][0])
+        PIDvy5 = drone5PIDy.update(pos_curr[4][1], location[4][1])
+        PIDvz5 = drone5PIDz.update(pos_curr[4][2], location[4][2])
+        #print("DROne 1: ", PIDvx1, PIDvy1, PIDvz1)
+        
+        PIDvx6 = drone6PIDx.update(pos_curr[5][0], location[5][0])
+        PIDvy6 = drone6PIDy.update(pos_curr[5][1], location[5][1])
+        PIDvz6 = drone6PIDz.update(pos_curr[5][2], location[5][2])
+        #print("DROne 2: ", PIDvx2, PIDvy2, PIDvz2)
+        
+        PIDvx7 = drone7PIDx.update(pos_curr[6][0], location[6][0])
+        PIDvy7 = drone7PIDy.update(pos_curr[6][1], location[6][1])
+        PIDvz7 = drone7PIDz.update(pos_curr[6][2], location[6][2])
+        #print("DROne 3: ", PIDvx3, PIDvy3, PIDvz3)
+    
+#        PIDvx8 = drone8PIDx.update(pos_curr[7][0], location[7][0])
+#        PIDvy8 = drone8PIDy.update(pos_curr[7][1], location[7][1])
+#        PIDvz8 = drone8PIDz.update(pos_curr[7][2], location[7][2])
         #print("DROne 4: ", PIDvx4, PIDvy4, PIDvz4)
         
         
@@ -200,6 +271,11 @@ try:
         allDrones[2].cmdVelocity(PIDvx3, PIDvy3, PIDvz3, 0)
         allDrones[3].cmdVelocity(PIDvx4, PIDvy4, PIDvz4, 0)
         
+        allDrones[4].cmdVelocity(PIDvx5, PIDvy5, PIDvz5, 0)
+        allDrones[5].cmdVelocity(PIDvx6, PIDvy6, PIDvz6, 0)
+        allDrones[6].cmdVelocity(PIDvx7, PIDvy7, PIDvz7, 0)
+#        allDrones[7].cmdVelocity(PIDvx8, PIDvy8, PIDvz8, 0)
+#        
         print(j)
         j+=1   
         rosClock.sleepForRate(1/Ts)           
